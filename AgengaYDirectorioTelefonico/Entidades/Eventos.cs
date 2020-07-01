8e2795fc734e37bbf14f449d754dba2ea9f3ee8a
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-07-01 19:03:30 -0400

    Clase eventos terminada

---
+++ AgengaYDirectorioTelefonico/Entidades/Eventos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgengaYDirectorioTelefonico.Entidades
+{
+    public class Eventos
+    {
+        [Key]
+        public int EventoId { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Nota { get; set; }
+
+        public Eventos()
+        {
+            EventoId = 0;
+            Descripcion = string.Empty;
+            Fecha = DateTime.Now;
+            Nota = string.Empty;
+        }
+    }
+}
