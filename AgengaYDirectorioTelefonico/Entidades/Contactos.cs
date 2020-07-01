6dc6e8456cef7e2e2fe18f6574b71f375b2a61be
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-07-01 18:58:58 -0400

    CLase Contactos terminada

---
+++ AgengaYDirectorioTelefonico/Entidades/Contactos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgengaYDirectorioTelefonico.Entidades
+{
+    public class Contactos
+    {
+        [Key]
+        public int ContactoId { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+        public string Nota { get; set; }
+
+        public Contactos()
+        {
+            ContactoId = 0;
+            Nombre = string.Empty;
+            Telefono = string.Empty;
+            Celular = string.Empty;
+            Correo = string.Empty;
+            Direccion = string.Empty;
+            Nota = string.Empty;
+        }
+    }
+}
