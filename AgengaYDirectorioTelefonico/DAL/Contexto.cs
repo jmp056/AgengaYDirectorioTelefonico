b7c0572c2ad00e8f1acfe0324186ab08818360d8
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-07-01 19:06:43 -0400

    Contexto terminado

---
+++ AgengaYDirectorioTelefonico/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using AgengaYDirectorioTelefonico.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgengaYDirectorioTelefonico.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Contactos>  Contactos { get; set; }
+        public DbSet<Eventos> Eventos { get; set; }
+
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
