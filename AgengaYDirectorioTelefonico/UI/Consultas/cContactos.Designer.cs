09fdf411695e87b947917e35333b3ffcb1b765d9
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-07-01 19:54:43 -0400

    Base de la consulta de Contactos terminada

---
+++ AgengaYDirectorioTelefonico/UI/Consultas/cContactos.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AgengaYDirectorioTelefonico.UI.Consultas
+{
+    partial class cContactos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FiltroLabel = new System.Windows.Forms.Label();
+            this.FiltroComboBox = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.AgregarButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CriterioLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FiltroLabel
+            // 
+            this.FiltroLabel.AutoSize = true;
+            this.FiltroLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FiltroLabel.Location = new System.Drawing.Point(37, 38);
+            this.FiltroLabel.Name = "FiltroLabel";
+            this.FiltroLabel.Size = new System.Drawing.Size(79, 16);
+            this.FiltroLabel.TabIndex = 0;
+            this.FiltroLabel.Text = "Filtrar por:";
+            // 
+            // FiltroComboBox
+            // 
+            this.FiltroComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FiltroComboBox.FormattingEnabled = true;
+            this.FiltroComboBox.Items.AddRange(new object[] {
+            "Nombre",
+            "Teléfono",
+            "Celular",
+            "Correo",
+            "Dirección",
+            "Nota"});
+            this.FiltroComboBox.Location = new System.Drawing.Point(161, 38);
+            this.FiltroComboBox.Name = "FiltroComboBox";
+            this.FiltroComboBox.Size = new System.Drawing.Size(121, 24);
+            this.FiltroComboBox.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(425, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // AgregarButton
+            // 
+            this.AgregarButton.Location = new System.Drawing.Point(558, 43);
+            this.AgregarButton.Name = "AgregarButton";
+            this.AgregarButton.Size = new System.Drawing.Size(173, 23);
+            this.AgregarButton.TabIndex = 3;
+            this.AgregarButton.Text = "Agregar nuevo contacto";
+            this.AgregarButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(691, 296);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // CriterioLabel
+            // 
+            this.CriterioLabel.AutoSize = true;
+            this.CriterioLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CriterioLabel.Location = new System.Drawing.Point(339, 46);
+            this.CriterioLabel.Name = "CriterioLabel";
+            this.CriterioLabel.Size = new System.Drawing.Size(62, 16);
+            this.CriterioLabel.TabIndex = 5;
+            this.CriterioLabel.Text = "Criterio:";
+            // 
+            // cContactos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CriterioLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.AgregarButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.FiltroComboBox);
+            this.Controls.Add(this.FiltroLabel);
+            this.Name = "cContactos";
+            this.Text = "cContactos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FiltroLabel;
+        private System.Windows.Forms.ComboBox FiltroComboBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button AgregarButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label CriterioLabel;
+    }
+}
