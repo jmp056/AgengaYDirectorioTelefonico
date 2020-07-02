45dbfa37be80ac16801a7eea7a28483ea98723da
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-07-01 22:28:49 -0400

    Hora agregada a la clase Eventos

--- AgengaYDirectorioTelefonico/Entidades/Eventos.cs
+++ AgengaYDirectorioTelefonico/Entidades/Eventos.cs
@@ -9,6 +9,7 @@ namespace AgengaYDirectorioTelefonico.Entidades
         public int EventoId { get; set; }
         public string Descripcion { get; set; }
         public DateTime Fecha { get; set; }
+        public int Hora{ get; set; }
         public string Nota { get; set; }
 
         public Eventos()
@@ -16,6 +17,7 @@ namespace AgengaYDirectorioTelefonico.Entidades
             EventoId = 0;
             Descripcion = string.Empty;
             Fecha = DateTime.Now;
+            Hora = 0;
             Nota = string.Empty;
         }
     }
